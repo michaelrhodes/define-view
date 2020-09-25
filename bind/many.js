8f05a179e5c0a81b6dd4c29e179439b281dbf990
michaelrhodes <spam@michaelrhod.es>
2020-09-25 23:00:12 +1000

    Add transform option to bind/many

--- bind/many.js
+++ bind/many.js
@@ -1,9 +1,15 @@
 module.exports = many
 
-function many (bindings) {
-  return function (v) {
+function many (transform, bindings) {
+  if (typeof transform !== 'function') {
+    bindings = transform
+    transform = null
+  }
+
+  return function (v, val) {
+    val = transform && v != null ? transform(v) : v
     for (var i = 0; i < bindings.length; i++) {
-      bindings[i].call(this, v)
+      bindings[i].call(this, val)
     }
   }
 }
