f3c835675eb5ae5a3837f18ae1cc30ee903ee444
michaelrhodes <spam@michaelrhod.es>
2021-02-25 09:40:20 +1000

    Remove test coupling

--- test/index.js
+++ test/index.js
@@ -5,7 +5,3 @@ assert('define exists', view.hasOwnProperty('define'))
 assert('bind exists', view.hasOwnProperty('bind'))
 assert('define is function', typeof view.define, 'function')
 assert('bind is an object', typeof view.bind, 'object')
-
-require('./define')
-require('./bind')
-require('./integration')
